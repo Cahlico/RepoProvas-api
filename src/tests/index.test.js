79cb400e798c4ad80283b4172f633463c16e2924
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-12-16 21:53:17 -0300

    adding test to post exams

--- src/tests/index.test.js
+++ src/tests/index.test.js
@@ -2,15 +2,28 @@ const db = require('../database');
 const supertest = require('supertest');
 const app = require('../app');
 
+async function resetDataBase() {
+    await db.query('DELETE FROM exams');
+}
+
+beforeAll(async () => {
+    resetDataBase();
+});
+
+afterAll(async () => {
+    resetDataBase();
+    db.end();
+});
+
 describe('GET /api/v1/subjects', () => {
     it('should return 200 with the correct objects', async () => {
         const response = await supertest(app).get('/api/v1/subjects');
 
         expect(response.status).toBe(200);
 
-        expect(response.body).toHaveProperty('id');
-        expect(response.body).toHaveProperty('name');
-        expect(response.body).toHaveProperty('period');
+        expect(response.body[0]).toHaveProperty('id');
+        expect(response.body[0]).toHaveProperty('name');
+        expect(response.body[0]).toHaveProperty('period');
     });
 });
 
@@ -22,7 +35,7 @@ describe('POST /api/v1/subjects/professor', () => {
 
         expect(response.status).toBe(200);
 
-        expect(response.body).toHaveProperty('name');
+        expect(response.body[0]).toHaveProperty('name');
     });
 
     it('should return 422 when the body is invalid', async () => {
@@ -32,4 +45,30 @@ describe('POST /api/v1/subjects/professor', () => {
 
         expect(response.status).toBe(422);
     })
+});
+
+describe('POST /api/v1/exams', () => {
+    it('should return 200 when the body is valid', async () => {
+        const body = {
+            link: 'https://www.respondeai.com.br/',
+            examType: "P3",
+            subjectId: 15
+        }
+
+        const response = await supertest(app).post('/api/v1/exams').send(body);
+
+        expect(response.status).toBe(200);
+    });
+
+    it('should return 422 when the body is invalid', async () => {
+        const body = {
+            link: 'https://www.respondeai.com.br/',
+            examType: "Prova 3",
+            subjectId: 15
+        }
+
+        const response = await supertest(app).post('/api/v1/exams').send(body);
+
+        expect(response.status).toBe(422);
+    });
 });
